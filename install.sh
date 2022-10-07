47134076fc620f84d30c55f8b1bc52a0a282a3ad
Casper Aangeenbrug <cas.aangeenbrug@gmail.com>
2022-10-07 20:55:32 +0200

    Add wallpapers to installation process

--- install.sh
+++ install.sh
@@ -7,5 +7,9 @@ mkdir -p ~/.local/share/fonts
 cp $DOTFILES/fonts/meslo-lgs-nf-fonts/*.ttf ~/.local/share/fonts
 cp $DOTFILES/fonts/fira-code/ttf/*.ttf ~/.local/share/fonts
 
+# Install wallpapers
+mkdir -p ~/Pictures
+git clone git@github.com:cassis163/wallpapers.git ~/Pictures/wallpapers
+
 # Finally, install configuration files with dotbot
 $DOTFILES/install
