59fd24fc8c073ef65a7d95403151eb660b4a0ed2
Casper Aangeenbrug <cas.aangeenbrug@gmail.com>
2022-10-10 16:23:02 +0200

    Remove weird polybar border

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -445,6 +445,7 @@ root.keys(globalkeys)
 awful.rules.rules = {
     -- All clients will match this rule.
     { rule = {},
+        except = { class = "Polybar" },
         properties = { border_width = beautiful.border_width,
             border_color = beautiful.border_normal,
             focus = awful.client.focus.filter,
