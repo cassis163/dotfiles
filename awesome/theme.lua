de622da9299e31125b3f93f9f8c3aae8ce439cd0
Casper Aangeenbrug <cas.aangeenbrug@gmail.com>
2022-10-10 16:23:25 +0200

    Change focus border color to mint green

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -28,7 +28,7 @@ theme.fg_minimize   = "#ffffff"
 theme.useless_gap   = dpi(3)
 theme.border_width  = dpi(1)
 theme.border_normal = "#000000"
-theme.border_focus  = "#535d6c"
+theme.border_focus  = "#00FF80"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
