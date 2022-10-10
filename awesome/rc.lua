e13ce160590b48bea746145c3a6cb9595fde94e8
Casper Aangeenbrug <cas.aangeenbrug@gmail.com>
2022-10-10 11:11:18 +0200

    Make polybar run on startup

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -565,5 +565,8 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
 
+-- Run polybar
+awful.util.spawn("sh ~/.config/polybar/launch.sh")
+
 -- Run picom
 awful.util.spawn("picom --experimental-backends -b")
